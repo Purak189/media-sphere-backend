3b101f7b63d5c291d5343f2788fcd24609dd93fb
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-10-29 09:31:46 -0500

    chore: first commit, initial configuration

---
+++ src/main/java/com/acme/mediaspherebackend/MediaSphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.mediaspherebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaSphereBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaSphereBackendApplication.class, args);
+    }
+
+}
